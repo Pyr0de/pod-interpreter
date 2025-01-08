3beb1b2968676c96ee821de496f4dc09de09ad27
Pyrode <pyrode.01@gmail.com>
2025-01-08 20:56:06 +0530

    Added single character tokens

--- cmd/pod-interpreter/scanner.go
+++ cmd/pod-interpreter/scanner.go
@@ -11,13 +11,33 @@ func Tokenize(input string) []token.Token {
 	tokens := []token.Token {}
 	line := 1
 	for i := 0; i < len(input); i++ {
-		switch input[i] {
+		start := i
+		c := input[i]
+
+		switch c {
 		case '(': 
-			tokens = append(tokens, token.Token{TokenType: token.L_BRACKET, Raw: "("})
+			tokens = append(tokens, token.Token{TokenType: token.L_BRACKET, Raw: input[start:i+1]})
 		case ')':
-			tokens = append(tokens, token.Token{TokenType: token.R_BRACKET, Raw: ")"})
+			tokens = append(tokens, token.Token{TokenType: token.R_BRACKET, Raw: input[start:i+1]})
+		case '+':
+			tokens = append(tokens, token.Token{TokenType: token.PLUS, Raw: input[start:i+1]})
+		case '-':
+			tokens = append(tokens, token.Token{TokenType: token.MINUS, Raw: input[start:i+1]})
+		case '*':
+			tokens = append(tokens, token.Token{TokenType: token.STAR, Raw: input[start:i+1]})
+		case ',':
+			tokens = append(tokens, token.Token{TokenType: token.COMMA, Raw: input[start:i+1]})
+		case ';':
+			tokens = append(tokens, token.Token{TokenType: token.SEMICOLON, Raw: input[start:i+1]})
+		//case (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_':
+		//	for (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' {
+
+		//	}
+		//	tokens = append(tokens, token.Token{TokenType: token.IDENTIFIER, Raw: input[start:i]})
+		case '\n':
+			line++
 		default: {
-			fmt.Fprintf(os.Stderr, "[line %d] Unexpected character: %s\n", line, string(input[i]))
+			fmt.Fprintf(os.Stderr, "[line %d] Unexpected character: %s\n", line, input[start:i+1])
 		}
 		}
 	}
