153d407d0f0689aa9d6c08b585b3003c9ab9982e
Pyrode <pyrode.01@gmail.com>
2025-01-08 22:01:37 +0530

    Added number, identifiers

--- cmd/pod-interpreter/scanner.go
+++ cmd/pod-interpreter/scanner.go
@@ -3,42 +3,128 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/Pyr0de/pod-interpreter/cmd/token"
 )
 
 func Tokenize(input string) []token.Token {
 	tokens := []token.Token {}
-	line := 1
+	//err := false
+
+	var line uint = 1
 	for i := 0; i < len(input); i++ {
 		start := i
 		c := input[i]
 
-		switch c {
-		case '(': 
-			tokens = append(tokens, token.Token{TokenType: token.L_BRACKET, Raw: input[start:i+1]})
-		case ')':
-			tokens = append(tokens, token.Token{TokenType: token.R_BRACKET, Raw: input[start:i+1]})
-		case '+':
-			tokens = append(tokens, token.Token{TokenType: token.PLUS, Raw: input[start:i+1]})
-		case '-':
-			tokens = append(tokens, token.Token{TokenType: token.MINUS, Raw: input[start:i+1]})
-		case '*':
-			tokens = append(tokens, token.Token{TokenType: token.STAR, Raw: input[start:i+1]})
-		case ',':
-			tokens = append(tokens, token.Token{TokenType: token.COMMA, Raw: input[start:i+1]})
-		case ';':
-			tokens = append(tokens, token.Token{TokenType: token.SEMICOLON, Raw: input[start:i+1]})
-		//case (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_':
-		//	for (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' {
+		if c >= '0' && c <= '9' {
+			i++
+			for (input[i] >= '0' && input[i] <= '9') || input[i] == '.' {
+				i++
+			}
+			i--
+			val, err := strconv.ParseFloat(input[start:i+1], 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "[line %d] Improper Number: %s\n", line, input[start:i+1])
+			}else {
+				tokens = append(tokens, token.Token{TokenType: token.NUMBER, Raw: input[start:i+1], Value: val, Line: line})
+			}
+			continue
+		}
+		if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' {
+			i++
+			for (input[i]>='A' && input[i]<='Z') || (input[i]>='a' && input[i]<='z') || 
+				(input[i]>='0' && input[i]<='9') || input[i]=='_' {
+				i++
+			}
+			i--
+			tokens = append(tokens, token.Token{TokenType: token.IDENTIFIER, Raw: input[start:i+1], Value: input[start:i+1], Line: line})
+			continue
+		}
 
-		//	}
-		//	tokens = append(tokens, token.Token{TokenType: token.IDENTIFIER, Raw: input[start:i]})
+		switch c {
+			case '(': 
+			tokens = append(tokens, token.Token{TokenType: token.L_BRACKET, Raw: input[start:i+1], Line: line})
+		case ')':
+			tokens = append(tokens, token.Token{TokenType: token.R_BRACKET, Raw: input[start:i+1], Line: line})
+		case '+':
+			tokens = append(tokens, token.Token{TokenType: token.PLUS, Raw: input[start:i+1], Line: line})
+		case '-':
+			tokens = append(tokens, token.Token{TokenType: token.MINUS, Raw: input[start:i+1], Line: line})
+		case '*':
+			tokens = append(tokens, token.Token{TokenType: token.STAR, Raw: input[start:i+1], Line: line})
+		case '^':
+			tokens = append(tokens, token.Token{TokenType: token.CARET, Raw: input[start:i+1], Line: line})
+		case ',':
+			tokens = append(tokens, token.Token{TokenType: token.COMMA, Raw: input[start:i+1], Line: line})
+		case ';':
+			tokens = append(tokens, token.Token{TokenType: token.SEMICOLON, Raw: input[start:i+1], Line: line})
+			//case (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_':
+			//	for (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' {
+
+			//	}
+			//	tokens = append(tokens, token.Token{TokenType: token.IDENTIFIER, Raw: input[start:i], Line: line})
+		case '=':
+			switch input[i+1] {
+			case '=':
+				i++
+				tokens = append(tokens, token.Token{TokenType: token.EQUAL_EQUAL, Raw: input[start:i+1], Line: line})
+			default:
+				tokens = append(tokens, token.Token{TokenType: token.EQUAL, Raw: input[start:i+1], Line: line})
+			}
+		case '!':
+			switch input[i+1] {
+			case '=':
+				i++
+				tokens = append(tokens, token.Token{TokenType: token.BANG_EQUAL, Raw: input[start:i+1], Line: line})
+			default:
+				tokens = append(tokens, token.Token{TokenType: token.BANG, Raw: input[start:i+1], Line: line})
+			}
+		case '>':
+			switch input[i+1] {
+			case '=':
+				i++
+				tokens = append(tokens, token.Token{TokenType: token.GREATER_EQUAL, Raw: input[start:i+1], Line: line})
+			default:
+				tokens = append(tokens, token.Token{TokenType: token.GREATER, Raw: input[start:i+1], Line: line})
+			}
+		case '<':
+			switch input[i+1] {
+			case '=':
+				i++
+				tokens = append(tokens, token.Token{TokenType: token.GREATER_EQUAL, Raw: input[start:i+1], Line: line})
+			default:
+				tokens = append(tokens, token.Token{TokenType: token.GREATER, Raw: input[start:i+1], Line: line})
+			}
+		case '/':
+			switch input[i+1] {
+			case '/':
+				for input[i] != '\n' && i < len(input) {
+					i++
+				}
+				i--
+			default:
+				tokens = append(tokens, token.Token{TokenType: token.SLASH, Raw: input[start:i+1], Line: line})
+			}
+		case '"', '\'': {
+			i++
+			for input[i] != c {
+				if input[i] == '\n' {
+					i--
+					fmt.Fprintf(os.Stderr, "[line %d] Unterminated String: %s\n", line, input[start:i+1])
+					break
+				}
+				i++
+			}
+			tokens = append(tokens, token.Token{TokenType: token.STRING, Raw: input[start:i+1], Value: input[start+1:i], Line: line})
+		}
+			
 		case '\n':
 			line++
 		default: {
 			fmt.Fprintf(os.Stderr, "[line %d] Unexpected character: %s\n", line, input[start:i+1])
-		}
+			//err = true
+			}
 		}
 	}
 
