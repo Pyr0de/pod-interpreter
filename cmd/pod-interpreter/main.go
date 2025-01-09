f2955233c25d8cc054bb7c76c43b446bc3d4ae1b
Pyrode <pyrode.01@gmail.com>
2025-01-09 21:59:24 +0530

    Completed parser for brackets

--- cmd/pod-interpreter/main.go
+++ cmd/pod-interpreter/main.go
@@ -8,38 +8,39 @@ import (
 func main() {
 	if len(os.Args) > 2 {
 		switch os.Args[1] {
-		case "tokenize": {
-			f, err := os.ReadFile(os.Args[2])
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
-				os.Exit(65)
+			case "tokenize": {
+				f, err := os.ReadFile(os.Args[2])
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
+					os.Exit(65)
+				}
+				t, err := Tokenize(string(f))
+				for _,v := range t {
+					v.Display()
+				}
+				if err != nil {
+					os.Exit(65)
+				}
+				return
 			}
-			t, err := Tokenize(string(f))
-			for _,v := range t {
-				v.Display()
-			}
-			if err != nil {
-				os.Exit(65)
-			}
-			return
-		}
-		case "parse": {
-			f, err := os.ReadFile(os.Args[2])
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
-				os.Exit(65)
-			}
-			t, err := Tokenize(string(f))
-			if err != nil {
-				os.Exit(65)
-			}
+			case "parse": {
+				f, err := os.ReadFile(os.Args[2])
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
+					os.Exit(65)
+				}
+				t, err := Tokenize(string(f))
+				if err != nil {
+					os.Exit(65)
+				}
 
-			for _, v := range Parse(t) {
-				fmt.Println(v.String())
+				for _, v := range Parse(t) {
+					fmt.Println(v)
+				}
+				return
 			}
 		}
-	}
-		
+
 	}
 	fmt.Println("Usage:", os.Args[0], "{tokenize/parse} {filename}")
 }
--- cmd/pod-interpreter/parser.go
+++ cmd/pod-interpreter/parser.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Pyr0de/pod-interpreter/cmd/group"
 	"github.com/Pyr0de/pod-interpreter/cmd/token"
 )
@@ -9,11 +11,43 @@ import (
 func Parse(tokens []token.Token) []group.Group {
 	exp := []group.Group{}
 	
-	var start_group group.Group = nil
+	var start_group *group.Group = nil
 	var curr_group *group.Group = nil
 
 	for _,k := range tokens {
-
+		switch k.TokenType {
+		case token.L_BRACKET: {
+			if curr_group != nil {
+				g := &group.Group{Parent: curr_group}
+				curr_group.AddGroup(g)
+				curr_group = g
+			}else {
+				start_group = &group.Group{}
+				curr_group = start_group
+			}
+		}
+		case token.R_BRACKET: {
+			if curr_group != nil {
+				if curr_group.Parent != nil {
+					curr_group = curr_group.Parent
+				}else {
+					exp = append(exp, *start_group)
+					curr_group = nil
+					start_group = nil
+				}
+			}else {
+				panic(fmt.Sprintf("[line %d] Invalid bracket", k.Line))
+			}
+		}
+		default: {
+			if curr_group != nil {
+				if curr_group.CanAdd(k) {
+					curr_group.AddToken(k)
+				}
+			}
+		}
+		}
 	}
 	return exp
 }
+
