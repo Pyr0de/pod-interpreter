337e3a1f8ebaef63fd7aa5df3ed79e84350792ce
Pyrode <pyrode.01@gmail.com>
2025-01-10 20:16:36 +0530

    partially working

--- cmd/pod-interpreter/parser.go
+++ cmd/pod-interpreter/parser.go
@@ -43,10 +43,45 @@ func Parse(tokens []token.Token) []group.Group {
 			if curr_group != nil {
 				if curr_group.CanAdd(k) {
 					curr_group.AddToken(k)
+				}else {
+					if k.TokenType == token.BANG {
+						g := &group.Group{Parent: curr_group}
+						curr_group.AddGroup(g)
+						curr_group = g
+						curr_group.AddToken(k)
+					}else if k.IsOperator(){
+						//g := curr_group
+						n := &group.Group{Parent: curr_group.Parent, Operand1: curr_group, Operator: k}
+						if n.Parent == nil {
+							start_group = n;
+							curr_group = n;
+						}else {
+							if curr_group == curr_group.Parent.Operand1 {
+								curr_group.Parent.Operand1 = n
+							}else if curr_group == curr_group.Parent.Operand2 {
+								curr_group.Parent.Operand2 = n
+							}
+						}
+					}else {
+						panic(fmt.Sprintf("Error: Current %s <- %s | %s", start_group, curr_group, k.Raw))
+					}
 				}
+			}else {
+				if k.IsOperand() || k.IsOperator() {
+					start_group = &group.Group{}
+					curr_group = start_group
+					curr_group.AddToken(k)
+				}
+			}
+
+			if (curr_group.Operator.TokenType == token.BANG && curr_group.Operand1 != nil) || curr_group.Operand2 != nil{
+				curr_group = curr_group.Parent
 			}
 		}
 		}
+	}
+	if start_group != nil {
+		exp = append(exp, *start_group)
 	}
 	return exp
 }
