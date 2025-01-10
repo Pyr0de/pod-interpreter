efda9826d6e3601ef38a1fa79c5e271793fd0a70
Pyrode <pyrode.01@gmail.com>
2025-01-10 21:46:12 +0530

    infix to postfix implemented

--- cmd/pod-interpreter/parser.go
+++ cmd/pod-interpreter/parser.go
@@ -10,79 +10,59 @@ import (
 
 func Parse(tokens []token.Token) []group.Group {
 	exp := []group.Group{}
+
+	//var start_group *group.Group = nil
+	//var curr_group *group.Group = nil
+
+	stack := []token.Token{}
+	result := []token.Token{}
 	
-	var start_group *group.Group = nil
-	var curr_group *group.Group = nil
-
-	for _,k := range tokens {
-		switch k.TokenType {
-		case token.L_BRACKET: {
-			if curr_group != nil {
-				g := &group.Group{Parent: curr_group}
-				curr_group.AddGroup(g)
-				curr_group = g
+	for _, k := range tokens {
+		if k.TokenType == token.SEMICOLON {
+			for len(stack) > 0 {
+				result = append(result, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			fmt.Println(result)
+			result = []token.Token {}
+		}else if k.IsOperand() {
+			result = append(result, k)
+		}else if k.TokenType == token.L_BRACKET {
+			stack = append(stack, k)
+		}else if k.TokenType == token.R_BRACKET {
+			for len(stack) > 0 && stack[len(stack)-1].TokenType != token.L_BRACKET {
+				result = append(result, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
 			}else {
-				start_group = &group.Group{}
-				curr_group = start_group
+				panic("Error in parsing expression")
 			}
-		}
-		case token.R_BRACKET: {
-			if curr_group != nil {
-				if curr_group.Parent != nil {
-					curr_group = curr_group.Parent
-				}else {
-					exp = append(exp, *start_group)
-					curr_group = nil
-					start_group = nil
-				}
-			}else {
-				panic(fmt.Sprintf("[line %d] Invalid bracket", k.Line))
+		}else {
+			for len(stack) > 0 && precedence(k) <= precedence(stack[len(stack)-1]) {
+				result = append(result, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
 			}
-		}
-		default: {
-			if curr_group != nil {
-				if curr_group.CanAdd(k) {
-					curr_group.AddToken(k)
-				}else {
-					if k.TokenType == token.BANG {
-						g := &group.Group{Parent: curr_group}
-						curr_group.AddGroup(g)
-						curr_group = g
-						curr_group.AddToken(k)
-					}else if k.IsOperator(){
-						//g := curr_group
-						n := &group.Group{Parent: curr_group.Parent, Operand1: curr_group, Operator: k}
-						if n.Parent == nil {
-							start_group = n;
-							curr_group = n;
-						}else {
-							if curr_group == curr_group.Parent.Operand1 {
-								curr_group.Parent.Operand1 = n
-							}else if curr_group == curr_group.Parent.Operand2 {
-								curr_group.Parent.Operand2 = n
-							}
-						}
-					}else {
-						panic(fmt.Sprintf("Error: Current %s <- %s | %s", start_group, curr_group, k.Raw))
-					}
-				}
-			}else {
-				if k.IsOperand() || k.IsOperator() {
-					start_group = &group.Group{}
-					curr_group = start_group
-					curr_group.AddToken(k)
-				}
-			}
-
-			if (curr_group.Operator.TokenType == token.BANG && curr_group.Operand1 != nil) || curr_group.Operand2 != nil{
-				curr_group = curr_group.Parent
-			}
-		}
+			
+			stack = append(stack, k)
 		}
 	}
-	if start_group != nil {
-		exp = append(exp, *start_group)
-	}
+
 	return exp
 }
 
+func precedence(t token.Token) uint{
+	if t.TokenType == token.BANG {
+		return 5
+	}else if t.TokenType == token.CARET {
+		return 4
+	}else if t.TokenType >= token.STAR && t.TokenType <= token.PERCENT {
+		return 3
+	}else if t.TokenType >= token.PLUS && t.TokenType <= token.MINUS {
+		return 2
+	}else if t.TokenType >= token.EQUAL && t.TokenType <= token.PIPE_PIPE {
+		return 1
+	}
+	return 0
+}
