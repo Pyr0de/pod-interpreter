0f2649b35ca4c611ff4e0dc55c1270f95d8c7abe
Pyrode <pyrode.01@gmail.com>
2025-01-12 13:28:20 +0530

    Added String for tokentype

--- cmd/token/token.go
+++ cmd/token/token.go
@@ -81,3 +81,82 @@ func (t Token)IsOperand() bool {
 func (t Token)IsUnary() bool {
 	return t.TokenType == BANG || t.TokenType == NEG 
 }
+
+func (t TokenType)String() string{
+	switch t {
+	case None:
+		return "None"
+	case L_BRACKET:
+		return "LEFT_PAREN"
+	case R_BRACKET:
+		return "RIGHT_PAREN"
+	case L_BRACE:
+		return "LEFT_BRACE"
+	case R_BRACE:
+		return "RIGHT_BRACE"
+	case SEMICOLON:
+		return "SEMICOLON"
+	case COMMA:
+		return "COMMA"
+	case EQUAL:
+		return "EQUAL"
+	case EQUAL_EQUAL:
+		return "EQUAL_EQUAL"
+	case BANG:
+		return "BANG"
+	case BANG_EQUAL:
+		return "BANG_EQUAL"
+	case LESS:
+		return "LESS"
+	case LESS_EQUAL:
+		return "LESS_EQUAL"
+	case GREATER:
+		return "GREATER"
+	case GREATER_EQUAL:
+		return "GREATER_EQUAL"
+	case AND_AND:
+		return "AND_AND"
+	case PIPE_PIPE:
+		return "PIPE_PIPE"
+	case PLUS:
+		return "PLUS"
+	case MINUS:
+		return "MINUS"
+	case NEG:
+		return "NEG"
+	case STAR:
+		return "STAR"
+	case SLASH:
+		return "SLASH"
+	case PERCENT:
+		return "PERCENT"
+	case CARET:
+		return "CARET"
+	case STRING:
+		return "STRING"
+	case NUMBER:
+		return "NUMBER"
+	case IDENTIFIER:
+		return "IDENTIFIER"
+	case FALSE:
+		return "FALSE"
+	case TRUE:
+		return "TRUE"
+	case IF:
+		return "IF"
+	case ELSE:
+		return "ELSE"
+	case FOR:
+		return "FOR"
+	case WHILE:
+		return "WHILE"
+	case INIT:
+		return "INIT"
+	case FUNC:
+		return "FUNC"
+	case RETURN:
+		return "RETURN"
+	}
+	return ""
+}
+
