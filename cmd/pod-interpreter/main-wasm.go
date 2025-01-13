a9874dd6084add003b684d7aff4821c02724ba5b
Pyrode <pyrode.01@gmail.com>
2025-01-13 23:53:52 +0530

    Added wasm file

---
+++ cmd/pod-interpreter/main-wasm.go
@@ -0,0 +1,27 @@
+//go:build wasm
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+func run(this js.Value, p[]js.Value) any {
+	
+	t := p[0].String()
+	file := p[1].String()
+
+	err := Interpreter(t, file)
+	if err != 0 {
+		fmt.Println("Usage: {tokenize/parse} {file}")
+	}
+	return js.ValueOf(err)
+}
+
+func main() {
+    c := make(chan struct{}, 0)
+
+    js.Global().Set("run", js.FuncOf(run))
+
+    <-c
+}
--- cmd/pod-interpreter/main.go
+++ cmd/pod-interpreter/main.go
@@ -1,3 +1,4 @@
+//go:build !wasm
 package main
 
 import (
@@ -6,47 +7,18 @@ import (
 )
 
 func main() {
+	fmt.Println("Building Native")
+	e := 1
 	if len(os.Args) > 2 {
-		switch os.Args[1] {
-			case "tokenize": {
-				f, err := os.ReadFile(os.Args[2])
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
-					os.Exit(1)
-				}
-				t, err := Tokenize(string(f))
-				for _,v := range t {
-					v.Display()
-				}
-				fmt.Println("EOF  null")
-				if err != nil {
-					os.Exit(65)
-				}
-				return
-			}
-			case "parse": {
-				f, err := os.ReadFile(os.Args[2])
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
-					os.Exit(1)
-				}
-				t, err := Tokenize(string(f))
-				if err != nil {
-					os.Exit(65)
-				}
-
-				exp, err := Parse(t)
-				for _, v := range exp {
-					fmt.Println(v)
-				}
-				
-				if err != nil {
-					os.Exit(65)
-				}
-				return
-			}
+		f, err := os.ReadFile(os.Args[2])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not read file:", os.Args[2])
+			os.Exit(1)
 		}
-
+		e = Interpreter(os.Args[1],string(f))
 	}
-	fmt.Println("Usage:", os.Args[0], "{tokenize/parse} {filename}")
+	if e != 0 {
+		fmt.Println("Usage:", os.Args[0], "{tokenize/parse} {filename}")
+	}
+	os.Exit(e)
 }
