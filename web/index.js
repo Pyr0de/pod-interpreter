530cc36a78fac708b42c58376f52f1986519d193
Pyrode <pyrode.01@gmail.com>
2025-01-15 09:45:01 +0530

    Added font and added catppuccin theme

--- web/index.js
+++ web/index.js
@@ -1,52 +1,11 @@
-const go = new Go();
-let wasm;
-
 let output = document.getElementById("output");
 let tab = document.getElementById("output_tab");
 let input = document.getElementById("input")
+let run_button = document.getElementById("run")
 output.value = "";
-(async() => {
-	let resp = await fetch("main.wasm")
-	wasm = await resp.arrayBuffer()
-})();
 
-function stdout(string) {
-	if (tab.classList.contains("success") || tab.classList.contains("fail")) {
-		tab.className = "loading"
+document.addEventListener("keydown", (e) => {
+	if (e.key == "Enter" && e.ctrlKey) {
+		run_button.click()
 	}
-	if (string.trim() != "") {
-		output.value += string.trim() + "\n"
-	}
-}
-
-let decoder = new TextDecoder("utf-8")
-let outputBuf = ""
-globalThis.fs.writeSync = (_, buf) => {
-	outputBuf += decoder.decode(buf);
-	const nl = outputBuf.lastIndexOf("\n");
-	if (nl != -2) {
-		stdout(outputBuf.substring(0, nl))
-		//console.log(outputBuf.substring(-1, nl));
-		outputBuf = outputBuf.substring(nl);
-	}
-	return buf.length;
-}
-go.exit = (code) => {
-	stdout(`Exit code: ${code}`)
-	if (code == 0) {
-		tab.className = "success"
-	}else {
-		tab.className = "fail"
-	}
-}
-
-document.getElementById("run").addEventListener("click", () => {
-	if (wasm == undefined || output == undefined) {
-		return
-	}
-	output.value = ""
-		WebAssembly.instantiate(wasm, go.importObject).then((result) => {
-		go.run(result.instance)
-		go.exit(run(document.getElementById("type").value.toLowerCase(), input.value))
-	})
-})
+}) 
---
+++ web/wasm.js
@@ -0,0 +1,64 @@
+const go = new Go();
+let wasm;
+(async() => {
+	let resp = await fetch("main.wasm")
+	wasm = await resp.arrayBuffer()
+})();
+
+function stdout(string) {
+	if (tab.classList.contains("success") || tab.classList.contains("fail")) {
+		tab.className = "loading"
+	}
+	if (string.trim() != "") {
+		output.innerHTML += string.trim() + "<br>"
+	}
+}
+
+function stderr(string) {
+	if (tab.classList.contains("success") || tab.classList.contains("fail")) {
+		tab.className = "loading"
+	}
+	if (string.trim() != "") {
+		let span = document.createElement("span")
+		span.innerText = string.trim() + "\n"
+		span.className = "err"
+		output.appendChild(span)
+	}
+}
+
+// program exit
+go.exit = (code) => {
+	if (code == 0) {
+		tab.className = "success"
+	}else {
+		stderr(`Exit code: ${code}`)
+		tab.className = "fail"
+	}
+};
+
+// stdout and stderr
+let decoder = new TextDecoder("utf-8");
+let outputBuf = "";
+globalThis.fs.writeSync = (fd, buf) => {
+	outputBuf += decoder.decode(buf);
+	const nl = outputBuf.lastIndexOf("\n");
+	if (nl != -2) {
+		let out = fd == 2 ? stderr : stdout
+		out(outputBuf.substring(0, nl))
+		outputBuf = outputBuf.substring(nl);
+	}
+	return buf.length;
+}
+
+// run program
+run_button.addEventListener("click", () => {
+	if (wasm == undefined || output == undefined) {
+		return
+	}
+	output.innerText = "";
+	output.innerHTML = "";
+	WebAssembly.instantiate(wasm, go.importObject).then((result) => {
+		go.run(result.instance)
+		go.exit(run(document.getElementById("type").value.toLowerCase(), input.value))
+	})
+})
