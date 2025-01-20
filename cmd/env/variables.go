429af4283fdbc93bebaa22f7a96ec8223a7642f4
Pyrode <pyrode.01@gmail.com>
2025-01-20 19:05:02 +0530

    Added scopes

--- cmd/env/variables.go
+++ cmd/env/variables.go
@@ -3,29 +3,52 @@ package env
 import "github.com/Pyr0de/pod-interpreter/cmd/token"
 
 
-var store map[string]token.Token = make(map[string]token.Token)
+var env *environment = &environment{store: make(map[string]token.Token), global: nil}
 
 func InitVar(variable string, val token.Token) {
-	store[variable] = val
+	env.store[variable] = val
 }
 
 func SetVar(variable string, val token.Token) bool {
-	if _, ok := store[variable]; ok {
-		store[variable] = val
+	e := findVar(variable)
+	if e != nil {
+		e.store[variable] = val
 		return false
 	}
 	return true
 }
 
 func DestructVar(variable string) {
-	if _, ok := store[variable]; ok {
-		delete(store, variable)
+	e := findVar(variable)
+	if e != nil {
+		delete(e.store, variable)
 	}
 }
 
 func GetVar(variable string) (token.Token, bool) {
-	if v, ok := store[variable]; ok {
-		return v, false
+	e := findVar(variable)
+	if e != nil {
+		t, _ := e.store[variable]
+		return t, false
 	}
 	return token.Token{}, true
+}
+
+type environment struct {
+	store map[string]token.Token
+	global *environment
+}
+
+func findVar(variable string) *environment {
+	curr := env
+
+	for curr != nil {
+		
+		if _, ok := curr.store[variable]; ok {
+			return curr
+		}
+
+		curr = curr.global
+	}
+	return nil
 }
