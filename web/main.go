10e963e7779085668cafc26858208509e486bc7a
Pyrode <pyrode.01@gmail.com>
2025-02-26 22:01:34 +0530

    Using bash file instead of make

--- web/main.go
+++ web/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./bin/web/")))
+    http.Handle("/", http.FileServer(http.Dir("./out/web/")))
 	log.Println("Starting Server on http://localhost:8080")
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
